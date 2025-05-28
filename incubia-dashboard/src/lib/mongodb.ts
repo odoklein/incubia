d4ca676b43f33b57d23fe278f4ba24ee7fc1ab9a
Slimani Mohammed <150338098+odoklein@users.noreply.github.com>
2025-05-28 10:30:04 +0100

    Update mongodb.ts

--- incubia-dashboard/src/lib/mongodb.ts
+++ incubia-dashboard/src/lib/mongodb.ts
@@ -1,20 +1,19 @@
 import { MongoClient } from 'mongodb'
 
 declare global {
-  // eslint-disable-next-line no-var
   var _mongoClientPromise: Promise<MongoClient> | undefined
 }
 
 const uri = process.env.MONGODB_URI!
 const options = {}
 
-let client: MongoClient
+const client: MongoClient = new MongoClient(uri, options)  // Use const here since it's not reassigned
 let clientPromise: Promise<MongoClient>
 
 if (!global._mongoClientPromise) {
-  client = new MongoClient(uri, options)
   global._mongoClientPromise = client.connect()
 }
+
 clientPromise = global._mongoClientPromise
 
 export default clientPromise
